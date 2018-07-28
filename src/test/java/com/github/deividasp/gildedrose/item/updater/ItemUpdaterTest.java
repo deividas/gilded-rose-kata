870d42666e3f4db5c72652c67d12f035bad5df2d
deividasp <deividas.popelskis@gmail.com>
2018-07-28 21:20:02 +0300

    Add a test for each implemented item updater

--- src/test/java/com/github/deividasp/gildedrose/item/updater/ItemUpdaterTest.java
+++ src/test/java/com/github/deividasp/gildedrose/item/updater/ItemUpdaterTest.java
@@ -1,5 +1,6 @@
 package com.github.deividasp.gildedrose.item.updater;
 
+import com.github.deividasp.gildedrose.item.TestItemDefinition;
 import com.github.deividasp.gildedrose.item.Item;
 import org.junit.Test;
 
@@ -38,6 +39,55 @@ public class ItemUpdaterTest {
 
         itemUpdater.setQuality(15);
         assertEquals(15, item.quality);
+    }
+
+    @Test
+    public void testAgedBrieItemUpdater() {
+        TestItemDefinition def = TestItemDefinition.AGED_BRIE_V1;
+        Item item = def.toItem();
+
+        testUpdater(item, def, new AgedBrieItemUpdater(item));
+    }
+
+    @Test
+    public void testBackstagePassesItemUpdater() {
+        TestItemDefinition def = TestItemDefinition.BACKSTAGE_PASSES_V1;
+        Item item = def.toItem();
+
+        testUpdater(item, def, new BackstagePassesItemUpdater(item));
+    }
+
+    @Test
+    public void testSulfurasItemUpdater() {
+        TestItemDefinition def = TestItemDefinition.SULFURAS_V1;
+        Item item = def.toItem();
+
+        testUpdater(item, def, new SulfurasItemUpdater(item));
+    }
+
+    @Test
+    public void testConjuredItemUpdater() {
+        TestItemDefinition def = TestItemDefinition.CONJURED_MANA_CAKE_V1;
+        Item item = def.toItem();
+
+        testUpdater(item, def, new ConjuredItemUpdater(item));
+    }
+
+    @Test
+    public void testDefaultItemUpdater() {
+        TestItemDefinition def = TestItemDefinition.DEXTERITY_VEST_V1;
+        Item item = def.toItem();
+
+        testUpdater(item, def, new DefaultItemUpdater(item));
+    }
+
+    private void testUpdater(Item item, TestItemDefinition def, ItemUpdater updater) {
+        updater.execute();
+
+        assertEquals("Incorrect sell-in for: " + def.toString(),
+                def.getExpectedSellIn(), item.sellIn);
+        assertEquals("Incorrect quality for: " + def.toString(),
+                def.getExpectedQuality(), item.quality);
     }
 
 }
