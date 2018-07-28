10ae263247d4822e6c5a0272b4afd016b4ea965a
deividasp <deividas.popelskis@gmail.com>
2018-07-28 13:00:04 +0300

    Add item types enumeration

---
+++ src/main/java/com/github/deividasp/gildedrose/ItemType.java
@@ -0,0 +1,24 @@
+package com.github.deividasp.gildedrose;
+
+import java.util.Arrays;
+
+public enum ItemType {
+
+    AGED_BRIE,
+    BACKSTAGE_PASSES,
+    SULFURAS,
+    CONJURED,
+    OTHER;
+
+    public String getName() {
+        return name().toLowerCase().replace("_", " ");
+    }
+
+    public static ItemType findBy(String itemName) {
+        return Arrays.stream(values())
+                .filter(type -> type != OTHER && itemName.toLowerCase().startsWith(type.getName()))
+                .findFirst()
+                .orElse(OTHER);
+    }
+
+}
