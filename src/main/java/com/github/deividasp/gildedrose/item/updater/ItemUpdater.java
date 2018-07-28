1fe05c428a8025717d4057988a3d768d24c319ea
deividasp <deividas.popelskis@gmail.com>
2018-07-28 20:59:11 +0300

    Add base class for item updating

---
+++ src/main/java/com/github/deividasp/gildedrose/item/updater/ItemUpdater.java
@@ -0,0 +1,56 @@
+package com.github.deividasp.gildedrose.item.updater;
+
+import com.github.deividasp.gildedrose.item.Item;
+
+public abstract class ItemUpdater {
+
+    private static final int MIN_QUALITY = 0;
+    private static final int MAX_QUALITY = 50;
+
+    private final Item item;
+
+    ItemUpdater(Item item) {
+        this.item = item;
+    }
+
+    public abstract void execute();
+
+    void decreaseSellIn() {
+        item.sellIn--;
+    }
+
+    boolean isSellInLessThan(int value) {
+        return item.sellIn < value;
+    }
+
+    boolean isSellInNegative() {
+        return item.sellIn < 0;
+    }
+
+    void setQuality(int quality) {
+        item.quality = quality;
+    }
+
+    void increaseQuality() {
+        increaseQuality(1);
+    }
+
+    void increaseQuality(int increaseBy) {
+        item.quality += increaseBy;
+
+        if (item.quality > MAX_QUALITY)
+            item.quality = MAX_QUALITY;
+    }
+
+    void decreaseQuality() {
+        decreaseQuality(1);
+    }
+
+    void decreaseQuality(int decreaseBy) {
+        item.quality -= decreaseBy;
+
+        if (item.quality < MIN_QUALITY)
+            item.quality = MIN_QUALITY;
+    }
+
+}
